2f956b27da1ed6198292a69670cf4cf88850a02a
Giada Unger <giada.unger02@gmail.com>
2024-02-15 17:29:17 +0100

    Did the layoute

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './index.css'
 
+import Layoute from './pages/Layoute';
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layoute />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
